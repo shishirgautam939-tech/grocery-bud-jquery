279e8efd0a81fbcbba74a3bf32e65b9d57a7ac4d
shishirgautam939-tech <shishirgautam939@gmail.com>
2026-02-11 21:20:37 +0545

    create app.js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var items = groceryItems;
+
+function render() {
+  var $app = $("#app");
+  $app.empty();
+
+  var $itemsElement = createItems(items);
+  $app.append($itemsElement);
+}
+
+$(document).ready(function () {
+  render();
+});
