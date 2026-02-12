9df0a30b4923d3436ac68fbbffdc092a9d07eb48
shishirgautam939-tech <shishirgautam939@gmail.com>
2026-02-12 10:05:41 +0545

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 var items = groceryItems;
 
+function editCompleted(itemId) {
+  items = $.map(items, function (item) {
+    if (item.id === itemId) {
+      return $.extend({}, item, { completed: !item.completed });
+    }
+    return item;
+  });
+
+  render(); 
+}
+
 function render() {
   var $app = $("#app");
   $app.empty();
